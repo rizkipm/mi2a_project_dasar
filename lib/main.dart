85de8a2f51572315c3605b8b9ae6fc13db7948aa
Rizki Syaputra <udarizki2020@gmail.com>
2026-03-14 12:01:29 +0800

    update 14 mar

--- lib/main.dart
+++ lib/main.dart
@@ -34,7 +34,7 @@ class MyApp extends StatelessWidget {
         // tested with just a hot reload.
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: const PageUtama(),
+      home: const PageSimpleForm(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/page_simple_form.dart
+++ lib/pages/page_simple_form.dart
@@ -1,7 +1,21 @@
 import 'package:flutter/material.dart';
 
-class PageSimpleForm extends StatelessWidget {
+class PageSimpleForm extends StatefulWidget {
   const PageSimpleForm({super.key});
+
+  @override
+  State<PageSimpleForm> createState() => _PageSimpleFormState();
+}
+
+class _PageSimpleFormState extends State<PageSimpleForm> {
+
+  //ambil value dari widget input form
+  TextEditingController txtUsername = new TextEditingController();
+  TextEditingController txtPassword = new TextEditingController();
+
+  //get value --> type object
+  //object --> ke String
+
 
   @override
   Widget build(BuildContext context) {
@@ -26,11 +40,12 @@ class PageSimpleForm extends StatelessWidget {
           Container(
             margin: EdgeInsets.all(10),
             child: TextFormField(
+              controller: txtUsername,
               decoration: InputDecoration(
-                labelText:  'Username',
-                labelStyle: TextStyle(
-                  color: Colors.lightBlue
-                )
+                  labelText:  'Username',
+                  labelStyle: TextStyle(
+                      color: Colors.lightBlue
+                  )
               ),
             ),
           ),
@@ -40,6 +55,7 @@ class PageSimpleForm extends StatelessWidget {
             margin: EdgeInsets.all(10),
             child:
             TextFormField(
+              controller: txtPassword,
               obscureText: true,//biar bulat-bulat atau gak keliatan passwordnya
               decoration: InputDecoration(
                   labelText:  'Password',
@@ -55,7 +71,19 @@ class PageSimpleForm extends StatelessWidget {
           Container(
             width: 200,
             child: ElevatedButton(onPressed: (){
+
+              //convert objet to String
+              String nUsername = txtUsername.text.toString();
+              String nPassword = txtPassword.text.toString();
+
+              print("Username anda adalah "+ nUsername + " dan password : ${nPassword}");
+
               print("Anda klik button login");
+
+              //Latihan
+              //Pengecekan username dan password
+              //1, kalau username = admin, password admin --> akan pindah ke page utama
+              //2. kalau username atau password salah --> tidak bisa pindah dan ada print "username atau password salah"
             }, child: Text("Login"),),
           )
         ],
@@ -64,3 +92,4 @@ class PageSimpleForm extends StatelessWidget {
     );
   }
 }
+
